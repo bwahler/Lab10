4f55c86b214179afc915368787a0494d3e1289bf
Brian <bmwahler@gmail.com>
2018-10-23 23:30:05 -0400

    Lab10

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10Practice
+{     
+    class Circle
+    {
+        public const double PI = Math.PI;
+        private double radius;
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+
+        public Circle(double rad)
+        {
+            radius = rad;
+        }
+        public string FormatNumber(double x)
+        {
+            return $"{x,0:N2}";
+        }
+        public double CalculateCircumference()
+        {
+            double circumference = PI * (radius * 2);
+            return circumference;
+        }
+        //Allow for user input on number of decimal places
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+        public double CalculateArea()
+        {
+            double area = PI * (radius * radius);
+            return area;
+        }
+        public string CalculateFormattedArea()
+        {
+            string formattedArea = FormatNumber(CalculateArea());
+            return formattedArea;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the radius of the circle!");
+                    double userInput = double.Parse(Console.ReadLine());
+
+                    Circle myCircle = new Circle(userInput);
+                    Console.WriteLine($"Circumference: {myCircle.CalculateCircumference()}");
+                    Console.WriteLine($"Circumference Formatted! {myCircle.CalculateFormattedCircumference()}");
+                    Console.WriteLine($"Area: {myCircle.CalculateArea()}");
+                    Console.WriteLine($"Area Formatted: {myCircle.CalculateFormattedArea()}");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Format is invalid, please try again");
+                }
+
+                Console.WriteLine("Would you like to keep going? (y/n)");
+                string userContinue = Console.ReadLine().ToLower();
+                if (userContinue == "y" || userContinue == "Y")
+                {
+                    continue;
+                }
+                else if (userContinue == "n" || userContinue == "N")
+                {
+                    Console.WriteLine("Goodbye. You have created circle objects");
+                    Console.ReadKey();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, I did not understand that! Try again (y/n)");
+                    userContinue = Console.ReadLine().ToLower();
+                }
+            }
+        }
+    }
+}
